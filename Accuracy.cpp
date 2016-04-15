a6c9ab10358877e5da5511f41ffd662e826edf7b
daniellebeckley <beckley.52@osu.edu>
2016-04-14 20:38:49 -0400

    finished Accurracy.cpp with test files

---
+++ Accuracy.cpp
@@ -0,0 +1,56 @@
+//Accuracy.cpp
+//compares the two label files
+//Currently prints out final accuracy
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void read_in_file(string filename, vector<int> &vector);
+double compare(vector<int> predicted, vector<int> truth);
+
+int main(int argc, char* argv[]){
+	string predLabelFile = argv[1];
+	string trueLabelFile= argv[2];
+
+	vector<int> predicted;
+	vector<int> truth;
+	read_in_file(predLabelFile, predicted);
+	read_in_file(trueLabelFile, truth);
+
+	double accuracy = compare(predicted, truth);
+	cout << "Accuracy:" << accuracy*100 << "%" << endl;
+
+return 0;
+}
+
+//reads in values from  file
+void read_in_file(string filename, vector<int> &vector){
+ 	ifstream in(filename.c_str());
+  	string line; 	
+  	if(in.is_open()){
+    while(getline(in,line)){
+      vector.push_back(stoi(line));	 
+    }
+    in.close();
+  }
+  else{ cout << "ERROR:: unable to open file!" << endl;}
+}
+//returns the accuracy
+double compare(vector<int> predicted, vector<int> truth){
+	int correct = 0;
+	int total = 0;
+	while(predicted.size() > 0 && truth.size() > 0){
+		if(predicted.back() == truth.back()){
+			correct++;
+		}
+		total++;
+		predicted.pop_back();
+		truth.pop_back();
+	}
+	double accuracy = (double)correct/total;
+	return accuracy;
+}
--- TrainLogReg.cpp
+++ TrainLogReg.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-void read_training_label(string filename, vector<string> &training_label);
-void read_training_feature(string filename, vector<string> &training_feature);
+void read_training_label(string filename, vector<int> &training_label);
+void read_training_feature(string filename, vector<int> &training_feature);
 int main(int argc, char* argv[]){
 	//string training_feature = arg[1];
 	//string training_label = arg[2];
@@ -14,8 +14,8 @@ int main(int argc, char* argv[]){
 	//int training_one = arg[4];
 	//int training_two = arg[5]; 
 
-  vector<string> training_label;
-  vector<string> training_feature;
+  vector<int> training_label;
+  vector<int> training_feature;
 
   read_training_label("trainingLabel.dat", training_label);
   read_training_feature("trainingFeature.dat", training_feature);
@@ -26,21 +26,21 @@ int main(int argc, char* argv[]){
  
 return 0;
 }
-void read_training_label(string filename, vector<string>& training_label){
+void read_training_label(string filename, vector<int>& training_label){
   ifstream in(filename.c_str());
   string line; 	
   if(in.is_open()){
     
     while(getline(in,line)){
       
-      training_label.push_back(line);	 
+      training_label.push_back(stoi(line));	 
     }
     in.close();
   }
   else{ cout << "unable to open file!" << endl;}
 }
 
-void read_training_feature(string filename, vector<string> &training_feature){
+void read_training_feature(string filename, vector<int> &training_feature){
   ifstream in(filename.c_str());
   string line;
   if(in.is_open()){
@@ -49,7 +49,7 @@ void read_training_feature(string filename, vector<string> &training_feature){
       for(int i = 0; i < line.length(); i++){
 	if(line.at(i) == ' '){
 	  string current = line.substr(start, i);
-	  training_feature.push_back(current);
+	  training_feature.push_back(stoi(current));
 	  start = i;
 	}
       }
